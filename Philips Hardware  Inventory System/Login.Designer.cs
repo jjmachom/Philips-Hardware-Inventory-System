4c83067cff51a9afac706c4e008d91ede4e82d2b
jjmachom <jjmacgroup@gmail.com>
2020-05-04 13:32:43 -0500

    Update Login.Designer.cs

--- Philips Hardware  Inventory System/Login.Designer.cs
+++ Philips Hardware  Inventory System/Login.Designer.cs
@@ -38,27 +38,27 @@
             // 
             // username
             // 
-            this.username.Location = new System.Drawing.Point(205, 408);
-            this.username.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.username.Location = new System.Drawing.Point(102, 211);
+            this.username.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(500, 38);
+            this.username.Size = new System.Drawing.Size(252, 22);
             this.username.TabIndex = 0;
             // 
             // pass
             // 
-            this.pass.Location = new System.Drawing.Point(205, 579);
-            this.pass.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.pass.Location = new System.Drawing.Point(102, 299);
+            this.pass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pass.Name = "pass";
-            this.pass.Size = new System.Drawing.Size(500, 38);
+            this.pass.Size = new System.Drawing.Size(252, 22);
             this.pass.TabIndex = 1;
             // 
             // login_btn
             // 
             this.login_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.login_btn.Location = new System.Drawing.Point(301, 715);
-            this.login_btn.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.login_btn.Location = new System.Drawing.Point(150, 369);
+            this.login_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.login_btn.Name = "login_btn";
-            this.login_btn.Size = new System.Drawing.Size(312, 124);
+            this.login_btn.Size = new System.Drawing.Size(156, 64);
             this.login_btn.TabIndex = 2;
             this.login_btn.Text = "LOGIN";
             this.login_btn.UseVisualStyleBackColor = true;
@@ -68,10 +68,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(275, 917);
-            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Location = new System.Drawing.Point(138, 473);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(375, 39);
+            this.label1.Size = new System.Drawing.Size(196, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Phillips Hardware IMS";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -80,23 +80,24 @@
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Image = global::Philips_Hardware__Inventory_System.Properties.Resources.logo_master;
-            this.pictureBox1.Location = new System.Drawing.Point(121, 85);
+            this.pictureBox1.Location = new System.Drawing.Point(123, 27);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(734, 285);
+            this.pictureBox1.Size = new System.Drawing.Size(220, 140);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 978);
+            this.ClientSize = new System.Drawing.Size(492, 505);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.login_btn);
             this.Controls.Add(this.pass);
             this.Controls.Add(this.username);
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
